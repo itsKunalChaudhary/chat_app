060f97c75c668d62726cb0dff331c329d1e10d6f
itsKunalChaudhary <kunal59chaudhary@gmail.com>
2025-07-16 14:52:05 +0530

    Added DB Access and Signup Routes using POST methods

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,16 @@
 import express from 'express';
+import dotenv from 'dotenv';
 import authRoutes from "./routes/route.auth.js"
+import {connectDB} from "./lib/db.js";
 const app = express();
+dotenv.config();
 
+const PORT = process.env.PORT;
+
+app.use(express.json());
 app.use("/api/auth", authRoutes);
 
-app.listen(5001, () => {
-    console.log('Server is running on port 5001');
+app.listen(PORT, () => {
+    console.log('Server is running on Port:'+ PORT);
+    connectDB();
 });
