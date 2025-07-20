7b6d33691a84c70ee9fc1c5c8d208ee8b7c4e3eb
itsKunalChaudhary <kunal59chaudhary@gmail.com>
2025-07-20 15:43:13 +0530

    Created Pages and Protected Routes to each Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,39 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import Navbar from "./components/Navbar.jsx";
+import {Navigate, Route, Routes} from "react-router-dom";
+import HomePage from "./pages/HomePage.jsx";
+import SignUpPage from "./pages/SignUpPage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+import SettingsPage from "./pages/SettingsPage.jsx";
+import ProfilePage from "./pages/ProfilePage.jsx";
+import * as axiosInstance from "autoprefixer";
+import {useAuthStore} from "./store/useAuthStore.js";
+import { Loader } from "lucide-react";
 
 const App = () => {
-    return (
-        <div className="text-3xl font-bold text-blue-600">
-            Tailwind v3 is working!
+    const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
+    useEffect(() => {
+        checkAuth();
+    }, [checkAuth()]);
+
+    console.log({ authUser });
+    if(isCheckingAuth && !authUser) return (
+        <div className={"flex items-center justify-center h-screen"}>
+            <Loader className={"size-10 animate-spin"} />
         </div>
     )
+
+    return (
+        <div>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login" />} />
+                <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to="/" />} />
+                <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
+                <Route path="/settings" element={<SettingsPage />} />
+                <Route path="/profile" element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
+            </Routes>
+        </div>
+    );
 }
 export default App
